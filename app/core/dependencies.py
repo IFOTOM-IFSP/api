1e2f4e2fdab65bdffc1a8bccc1076b740f063bbc
Lucas Biazon <129486114+LucasBiazon@users.noreply.github.com>
2025-10-21 21:18:29 -0300

    Refactor import statement for SpectraProcessor

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 from functools import lru_cache
-from app.core.image_processor import SpectraProcessor
+
+from .image_processor import SpectraProcessor
 
 @lru_cache(maxsize=1)
 def get_spectra_processor() -> SpectraProcessor:
